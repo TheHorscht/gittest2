98e66cb45789b535d25e070d8255076d5fb5f8fc
The Horscht <realhorscht+github@gmail.com>
2021-11-22 14:47:04 +0100

    Add some line

--- s.py
+++ s.py
@@ -2,5 +2,8 @@ import subprocess
 
 process = subprocess.run(['git', 'tag', '--points-at', 'HEAD'],
   stdout=subprocess.PIPE)
+
+# Some line
+
 output = process.stdout.decode('utf-8').strip()
 print(output == '')
