25b859dc860be5271ca75bdee085c53320595e99
The Horscht <realhorscht+github@gmail.com>
2021-11-22 14:49:13 +0100

    Third change on bran

--- s.py
+++ s.py
@@ -3,7 +3,7 @@ import subprocess
 process = subprocess.run(['git', 'tag', '--points-at', 'HEAD'],
   stdout=subprocess.PIPE)
 
-# Second change on bran
+# Third change on bran
 
 output = process.stdout.decode('utf-8').strip()
 print(output == '')
