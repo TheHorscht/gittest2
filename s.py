66c62a3b511c3c10a25f6aa8320c1bfcea2819a6
The Horscht <realhorscht+github@gmail.com>
2021-11-22 14:47:34 +0100

    Third change on master

--- s.py
+++ s.py
@@ -3,7 +3,7 @@ import subprocess
 process = subprocess.run(['git', 'tag', '--points-at', 'HEAD'],
   stdout=subprocess.PIPE)
 
-# Second change on master
+# Third change on master
 
 output = process.stdout.decode('utf-8').strip()
 print(output == '')
